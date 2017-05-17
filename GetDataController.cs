ae8f84723e16cb2ad968386bb830c5b1ccfff64f
ab02111996 <ab02111996@yandex.ru>
2017-05-17 20:15:20 +0700

    Add files via upload

---
+++ GetDataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class GetDataController : ApiController
+    {
+        public XElement GetXml(WeightModels w)
+        {
+            var res = new XElement
+                ("Item",
+                    new XElement("TitleOfItem", w.titleOfItem),
+                    new XElement("Date", w.date),
+                    new XElement("Weight", w.weight),
+                    new XElement("UnitOfWeight", w.unitOfWeight)
+                );
+            return res;
+        }
+
+        public string GetJson(WeightModels w)
+        {
+            string res = JsonConvert.SerializeObject(w);
+            return res;
+        }
+    }
+}
+
+
---
+++ WeightModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class WeightModels
+    {
+        public string titleOfItem;
+        public string date;
+        public double weight;
+        public string unitOfWeight;
+
+        public WeightModels() { }
+    }
+}
