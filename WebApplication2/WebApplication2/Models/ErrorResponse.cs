7c5267db93320a602a465f318a487af45bd2879a
parshikovalexey <parshikovalexey@gmail.com>
2017-06-22 16:05:23 +0700

    Добавил проект из архива Артема.

---
+++ WebApplication2/WebApplication2/Models/ErrorResponse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public enum ErrorCodes
+    {
+        InvalidWeightId = 1,
+        WeightNotFound = 2,
+        InvalidWeightModel = 3,
+        SaveWeightError = 4
+    }
+
+
+    public class ErrorResponse
+    {
+        public ErrorCodes ErrorCode { get; set; }
+        public string Message { get; set; }
+        public int Index { get; set; }
+
+        public ErrorResponse()
+        {
+        }
+
+        public ErrorResponse(ErrorCodes ErrorCode, string Message, int IndexOfError) // добавил конструктор
+        {
+            this.ErrorCode = ErrorCode;
+            this.Message = Message;
+        }
+
+        public ErrorResponse(ErrorCodes ErrorCode, string Message)
+        {
+            this.ErrorCode = ErrorCode;
+            this.Message = Message;
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/WeightModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class WeightModels
+    {
+        public string TitleOfItem { get; set; }
+        public DateTime Date { get; set; }
+        public double? Weight { get; set; }
+        public string UnitOfWeight { get; set; }
+        public int? WeightId { get; set; } // добавил в модель Id
+
+        public WeightModels() { }
+    }
+}
