8610ab28371c5c0d7ca904a276f1b3f7d5919163
Alexey Parshikov <parshikovalexey@gmail.com>
2017-07-05 12:14:06 +0700

    Добавил проект из архива Артема. (#2)

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            GetDataController c1 = new GetDataController();
+            var w1 = new WeightModels();
+            w1.Date = DateTime.Now;
+            w1.TitleOfItem = "Item";
+            w1.UnitOfWeight = "Unit";
+            w1.Weight = 20;
+            var v1 = c1.GetXml(w1);
+            var v2 = c1.GetJson(w1);
+
+            //HttpResponseMessage v3 = c1.GetWeightings();
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/ErrorResponse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public enum ErrorCodes
+    {
+        InvalidWeightId = 1,
+        WeightNotFound = 2,
+        InvalidWeightModel = 3,
+        SaveWeightError = 4,
+        InvalidUserInfoModel = 5,
+        InvalidUserID = 6,
+        ChangePasswordError = 7,
+        InvalidLikesModel = 8
+    }
+
+
+    public class ErrorResponse
+    {
+        public ErrorCodes ErrorCode { get; set; }
+        public string Message { get; set; }
+        public int Index { get; set; }
+
+        public ErrorResponse()
+        {
+        }
+
+        public ErrorResponse(ErrorCodes ErrorCode, string Message, int IndexOfError) // добавил конструктор
+        {
+            this.ErrorCode = ErrorCode;
+            this.Message = Message;
+        }
+
+        public ErrorResponse(ErrorCodes ErrorCode, string Message)
+        {
+            this.ErrorCode = ErrorCode;
+            this.Message = Message;
+        }
+    }
+}
