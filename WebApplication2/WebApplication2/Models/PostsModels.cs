622012f61eee04cb21b26afaa8d4986e1dbfb533
ab02111996 <ab02111996@yandex.ru>
2017-07-17 01:12:51 +0700

    Add files via upload
    исправил методы

---
+++ WebApplication2/WebApplication2/Models/PostsModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class PostsModels
+    {
+        public string Data { get; set; } // содержание поста
+        public DateTime Time { get; set; } // дата создания
+        public List <LikesModels> Likes { get; set; } // лайки
+        public int ID { get; set; } // идентификатор
+
+        public PostsModels() { }
+    }
+}
